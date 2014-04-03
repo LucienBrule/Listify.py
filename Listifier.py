b447455e79e8ef7fae71d5a43ed690f915f8d761
Lucien Brulé <lucienb@outlook.com>
2014-04-02 19:32:59 -0700

    Works now
    Changed it to actually be python

--- Listifier.py
+++ Listifier.py
@@ -1,28 +1,34 @@
-//String array listifier.py
+#String array listifier.py
 choice = 0;
-wordList = [];
+wordList = [""];
 word = "";
 sentinel = "Cheese balls";
 
+print("Listify.py\n");
+print("Enter a word then hit enter\n" + "\"" + sentinel + "\"" + " to exit");
+
 while(word != "Cheese balls"):
     word = raw_input();
-    wordList.append(("\'" + str(word) + "\'" + ","));
+    wordList.append("\'" + word + "\'" + ",");
+wordList.pop(len(wordList) -1);
 
 print("(1 dump file list to console), (2 write list to file), (3 do both), (any other: exit)");
 choice = raw_input();
 
-int(choice);
-if(choice == 1):
+choice = int(choice);
+print (choice);
+
+if choice == 1 :
     for w in wordList:
-        print(w)
-else if(choice == 2):
+        print(w);
+elif choice == 2 :
     fname = "";
     print("Enter path and fname from root");
     f = open(fname, "a");
     print f
     for w in wordList:
         f.write(w);
-else if(choice == 3):
+elif choice == 3:
     for w in wordList:
         print(w);
     fname = "";
